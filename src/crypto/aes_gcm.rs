4a038f4e2c5ae2bf17d8b85704bf388f9e1d0bbb
Monica Moniot <mamoniot@protonmail.com>
2023-07-25 11:34:29 -0400

    added docs

--- src/crypto/aes_gcm.rs
+++ src/crypto/aes_gcm.rs
@@ -4,6 +4,14 @@ pub const AES_GCM_TAG_SIZE: usize = 16;
 pub const AES_GCM_IV_SIZE: usize = 12;
 pub const AES_GCM_KEY_SIZE: usize = super::aes::AES_256_KEY_SIZE;
 
+/// The order of calls to this trait is always
+/// `set_iv` -> `set_aad` -> `encrypt` -> `finish_encrypt`.
+/// `set_aad` and `encrypt` may not always be called. `encrypt_in_place` may be called instead of
+/// `encrypt`. `encrypt` may be called multiple times, it should start encryption of the input at the point in
+/// the keystream where encryption previously ended.
+/// An instance of this trait may be reused multiple times, each reuse should use the same key.
+/// If it is reused, `set_iv` will always be the very next call after `finish_encrypt`.
+///
 /// Implementations of this trait does not have to be Send + Sync,
 /// but if it is wrapped in a `Mutex` it must satisfy the requirements of Send + Sync.
 pub trait AesGcmEnc {
@@ -20,6 +28,14 @@ pub trait AesGcmEnc {
     fn finish_encrypt(&mut self, output: &mut [u8; AES_GCM_TAG_SIZE]);
 }
 
+/// The order of calls to this trait is always
+/// `set_iv` -> `set_aad` -> `decrypt` -> `finish_decrypt`.
+/// `set_aad` and `decrypt` may not always be called. `decrypt_in_place` may be called instead of
+/// `decrypt`. `decrypt` may be called multiple times, it should start decryption of the input at the point in
+/// the keystream where decryption previously ended.
+/// An instance of this trait may be reused multiple times, each reuse should use the same key.
+/// If it is reused, `set_iv` will always be the very next call after `finish_decrypt`.
+///
 /// Implementations of this trait does not have to be Send + Sync,
 /// but if it is wrapped in a `Mutex` it must satisfy the requirements of Send + Sync.
 pub trait AesGcmDec {
--- src/crypto/p384.rs
+++ src/crypto/p384.rs
@@ -8,9 +8,14 @@ pub const P384_ECDH_SHARED_SECRET_SIZE: usize = 48;
 /// A NIST P-384 ECDH/ECDSA public key.
 pub trait P384PublicKey: Sized + Send + Sync {
     /// Create a p384 public key from raw bytes.
+    ///
+    /// **CRITICAL**: This function must return `None` if the input `raw_key` is not on the P384 curve,
+    /// or if it breaks the P384 standard in any other way.
     fn from_bytes(raw_key: &[u8; P384_PUBLIC_KEY_SIZE]) -> Option<Self>;
 
     /// Get the raw bytes that uniquely define the public key.
+    ///
+    /// This must output the standard 49 byte NIST encoding of P384 public keys.
     fn as_bytes(&self) -> &[u8; P384_PUBLIC_KEY_SIZE];
 }
 
@@ -24,8 +29,17 @@ pub trait P384KeyPair: Send + Sync {
     fn generate(rng: &mut Self::Rng) -> Self;
 
     /// Get the raw bytes that uniquely define the public key.
+    ///
+    /// This must output the standard 49 byte NIST encoding of P384 public keys.
     fn public_key_bytes(&self) -> &[u8; P384_PUBLIC_KEY_SIZE];
 
-    /// Perform ECDH key agreement, returning the raw (un-hashed!) ECDH secret.
+    /// Perform ECDH key agreement, writing the raw (un-hashed!) ECDH secret to `output`.
+    ///
+    /// **CRITICAL**: This function must return `false` if key agreement between this private key and
+    /// the input `other_public` key would result in an invalid, non-standard or predictable ECDH secret.
+    /// Please refer to the NIST spec for P384 ECDH key agreement, or better yet use a peer reviewed
+    /// library that has already implemented this correctly.
+    ///
+    /// If this function returns `false` then the contents of `output` will be discarded.
     fn agree(&self, other_public: &Self::PublicKey, output: &mut [u8; P384_ECDH_SHARED_SECRET_SIZE]) -> bool;
 }
